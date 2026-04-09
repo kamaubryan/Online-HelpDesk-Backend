d64f911ec7ed58d8b353767bb7e0bb7f4fae42ab
kamaubryan <briankamau344@gmail.com>
2026-04-09 05:16:03 +0300

    initial commit

---
+++ src/main/java/Online/Help/Desk/OnlineHelpDeskApplication.java
@@ -0,0 +1,13 @@
+package Online.Help.Desk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineHelpDeskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineHelpDeskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Online/Help/Desk/OnlineHelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package Online.Help.Desk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineHelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
